d20f452a7ef219b225251a3b5496bb3d64ac32cd
halflinghelper <calvinjosenhans10@gmail.com>
2024-06-08 10:52:21 -0400

    refactor and update readme.

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-# XKCD-DiscordBot
+# XKCD-Bot
 XKCD discord bot to get comics for your server.
+
+[Invite Link](https://discord.com/oauth2/authorize?client_id=697915545974210691&permissions=2048&integration_type=0&scope=bot+applications.commands)
 
 ## Usage
 `npm install`
 
 `npm start`
+
+## About
+I wrote this bot in high school during the pandemic, soon after I first learned
+about node.js. I learned the [discord.js](https://discord.js.org/) module and
+figured out what `fetch` and API meant just barely enough to get a working bot
+(see [this](https://github.com/HalflingHelper/XKCD-DiscordBot/tree/d5c0c56b8b7f5296d3daa20511fd5e4c92fa137f)
+commit).
+
+This summer, over 4 years later, it became a fun weekend project to look back at
+and improve this questionable code, and see just how far I'd come in my 
+understanding of node, the web, and programming style.
+
+You can add XKCD bot to your server [here](https://discord.com/oauth2/authorize?client_id=697915545974210691&permissions=2048&integration_type=0&scope=bot+applications.commands)
--- bot.js
+++ bot.js
@@ -1,78 +1,105 @@
-require('dotenv').config()
-const { Client, MessageAttachment, GatewayIntentBits } = require('discord.js');
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ] 
-});
+  require('dotenv').config()
+  const { Client, GatewayIntentBits} = require('discord.js');
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent
+    ]
+  });
 
-const bot_secret_token = process.env.SECRET_TOKEN
+  const bot_secret_token = process.env.SECRET_TOKEN
 
-var canSend
-var maxComic;
-var lastComic = 'null';
+  const UPDATE_FREQ = 15 * 60 * 1000;; // 15 minutes
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  let state = {
+    maxComic: 2943,
+    lastComic: null,
+    lastUpdate: 0
+  }
 
-});
+  function getRandomId() {
+    return Math.floor(Math.random() * state.maxComic) + 1;
+  }
 
-client.on('messageCreate', async msg => {
-  if (msg.content.startsWith("XKCD")){
-    canSend = true
-    maxComic = 2295
-    
-    const { num } = await fetch(`https://xkcd.com/info.0.json`).then(response => response.json())
-  
-    maxComic = num
-    var ComicID;
-    var stuff = msg.content.split('?');
-    
-    if (stuff[1] == 'latest') {
-      ComicID = maxComic
-    } else if (stuff[1] == 'random') {
-      ComicID = (Math.floor(Math.random() * maxComic) + 1)
-      lastComic = ComicID
-      
-    } else if (stuff[1] == 'next') {
-      if (lastComic == 'null' || lastComic == maxComic) {
-        msg.channel.send("There isn't a next comic; here's a random comic for your trouble though.")
-        ComicID = (Math.floor(Math.random() * maxComic) + 1)
-        lastComic = ComicID
-      } else{
-        ComicID = lastComic + 1;
-        
+  async function getLatestComic() {
+    const { num } = await fetch('https://xkcd.com/info.0.json')
+      .then(response => response.json());
+    return num ? num : state.maxComic;
+  }
+
+  function getComicId(arg) {
+    let id = state.maxComic;
+    let err = null;
+
+    if (arg === 'latest') {
+      id = state.maxComic;
+    } else if (arg === 'random') {
+      id = getRandomId();
+    } else if (arg === 'next') {
+      if (!state.lastComic) {
+        id = getRandomId();
+        err = "There isn't a comic to compare to; here's a random one instead!";
+      } else {
+        id = state.lastComic % state.maxComic + 1;
       }
-    } else if (stuff[1] == 'last') {
-      if (lastComic == 'null' || lastComic == maxComic) {
-        msg.channel.send("There isn't a next comic; here's a random comic for your trouble though.")
-        ComicID = (Math.floor(Math.random() * maxComic) + 1)
-        lastComic = ComicID
-      } else{
-        ComicID = lastComic - 1;
-        
+    } else if (arg === 'previous') {
+      if (!state.lastComic) {
+        id = getRandomId();
+        err = "There isn't a comic to compare to; here's a random one instead!";
+      } else {
+        id = state.lastComic === 1 ? state.maxComic : state.lastComic - 1;
       }
     } else {
-      var numb = parseInt(stuff[1])
-      if (numb <= maxComic && numb > 0){
-        ComicID = numb
-        lastComic = ComicID
+      const num = parseInt(arg);
+      if (num <= state.maxComic && num > 0) {
+        id = num;
       } else {
-        msg.reply("That isn't a valid comic id there buddy; here's a random one instead!")
-        ComicID = (Math.floor(Math.random() * maxComic) + 1)
-        lastComic = ComicID
+        id = getRandomId();
+        err = "That isn't a valid comic id; here's a random one instead!";
       }
-      
     }
-    if (canSend) {  
-    fetch(`https://xkcd.com/` + ComicID + `/info.0.json`).then(res => res.json()).then(json => msg.channel.send(json.img))  
-      canSend = false;
+
+    return [id, err];
   }
-  }
-});
 
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
 
+  client.on('messageCreate', async msg => {
+    if (!msg.content.startsWith("XKCD?")) { return; }
 
-client.login(bot_secret_token)
+    const now = new Date();
+    if (now - state.lastUpdate > UPDATE_FREQ) {
+      state.lastUpdate = now;
+      state.maxComic = await getLatestComic();
+    }
+
+    const stuff = msg.content.split('?');
+    const arg = stuff[1] === "" ? "random" : stuff[1];
+
+    if (arg === 'help') {
+      msg.reply("XKCD Bot Usage:\n" +
+        "`XKCD?help` - Send this help message\n" +
+        "`XKCD?{num}` - Sends the `num`th XKCD. i.e. `XKCD?114`\n"  +
+        "`XKCD?` - Send a random XKCD Comic\n" +
+        "`XKCD?latest` - Send the most recent XKCD Comic\n" +
+        "`XKCD?next` - Display the comic after the most recently sent one\n" +
+        "`XKCD?previous` - Send the comic before the most recently sent one\n" +
+        "`XKCD?random` - Send a random XKCD Comic\n");
+    } else {
+      let [comicId, err] = getComicId(arg);
+
+      if (err) {
+        msg.reply(err);
+      }
+
+      state.lastComic = comicId;
+      fetch(`https://xkcd.com/${comicId}/info.0.json`)
+        .then(res => res.json())
+        .then(json => msg.reply(`XKCD ${comicId}: ${json.img}`));
+    }
+  });
+
+  client.login(bot_secret_token);
